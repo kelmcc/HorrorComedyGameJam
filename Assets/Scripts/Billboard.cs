1c72846dd8ca585a2230894fbcfe9a54acb26baf
Jeff <jeff@renderheads.com>
2022-02-19 19:23:40 +0200

    updated billboard

---
+++ Assets/Scripts/Billboard.cs
@@ -0,0 +1,29 @@
+//RenderHeads - Jeff Rusch
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RenderHeads
+{
+    public class Billboard : MonoBehaviour
+    {
+		#region Public Properties
+
+		#endregion
+
+		#region Private Properties
+
+		#endregion
+
+		#region Public Methods
+		public void Update()
+		{
+			this.transform.LookAt(Camera.main.transform, Vector3.up);
+		}
+		#endregion
+
+		#region Private Methods
+
+		#endregion
+	}
+}
